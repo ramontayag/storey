b1bab3938931878dae1dace239e48bd1a18b7d81
Ramon Tayag <ramon@tayag.net>
2012-10-03 20:56:49 +0800

    Format changes

--- spec/storey/hstore_spec.rb
+++ spec/storey/hstore_spec.rb
@@ -5,17 +5,16 @@ describe Storey::Hstore do
     it 'should install the extension into the hstore schema' do
       Storey.persistent_schemas = %w(hstore)
       described_class.install
-      expect {
-        ActiveRecord::Base.connection.execute "DROP EXTENSION hstore"
-      }.to_not raise_error(ActiveRecord::StatementInvalid)
+      expect { ActiveRecord::Base.connection.execute "DROP EXTENSION hstore" }.
+        to_not raise_error(ActiveRecord::StatementInvalid)
     end
 
     context 'when hstore is not one of the persistent schemas' do
       it 'should fail with an StoreyError' do
         Storey.persistent_schemas = []
-        expect {
-          described_class.install
-        }.to raise_error(Storey::StoreyError, 'You are attempting to install hstore data type, but the hstore schema (where the data type will be installed) is not one of the persistent schemas. Please add hstore to the list of persistent schemas.')
+        message = 'You are attempting to install hstore data type, but the hstore schema (where the data type will be installed) is not one of the persistent schemas. Please add hstore to the list of persistent schemas.'
+        expect { described_class.install }.
+          to raise_error(Storey::StoreyError, message)
       end
     end
   end

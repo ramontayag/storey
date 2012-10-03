513ece2786ce7253bc0ac67789520a0d8b337c3a
Ramon Tayag <ramon@tayag.net>
2012-10-03 22:05:30 +0800

    Formatting changes

--- spec/storey/schema_search_path_for_spec.rb
+++ spec/storey/schema_search_path_for_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe Storey, '.schema_search_path_for' do
+describe Storey, '#schema_search_path_for' do
+
   context 'given a blank argument' do
     it 'should return the default path' do
       Storey.switch
       Storey.schema.should == Storey.default_search_path
     end
   end
+
 end

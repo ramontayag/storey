cfdb2e69b5c20ab2f68a3ca2e2fe10c94fa76c41
Ramon Tayag <ramon.tayag@gmail.com>
2018-06-27 18:15:13 +0800

    Update RSpec usage

--- spec/storey/default_search_path_spec.rb
+++ spec/storey/default_search_path_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'Storey', '#default_search_path' do
 
-  it 'should include the persistent schemas' do
+  it 'includes the persistent schemas' do
     Storey.persistent_schemas = %w(hello there)
-    Storey.default_search_path.should == '"$user",public,hello,there'
+    expect(Storey.default_search_path).to eq '"$user",public,hello,there'
   end
 
   context 'when the persistent schemas includes `public`' do
-    it 'should only have one instance of public' do
+    it 'has one instance of public' do
       Storey.persistent_schemas = %w(public hello)
-      Storey.default_search_path.should == '"$user",public,hello'
+      expect(Storey.default_search_path).to eq '"$user",public,hello'
     end
   end
 

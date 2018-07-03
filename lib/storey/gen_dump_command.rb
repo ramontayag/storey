70079f7280829e448ba9252655b55592cfbba4ab
Ramon Tayag <ramon.tayag@gmail.com>
2018-07-03 12:13:16 +0800

    Simplify GenDumpCommand to not use instances of itself
    - Remove reliance on `easy_class_to_instance_method`

--- lib/storey/gen_dump_command.rb
+++ lib/storey/gen_dump_command.rb
@@ -1,39 +1,34 @@
 module Storey
   class GenDumpCommand
 
-    easy_class_to_instance
-
-    def initialize(options={})
-      @options = options
-      if @options[:database].blank?
+    def self.execute(options={})
+      if options[:database].blank?
         raise ArgumentError, 'database must be supplied'
       end
-    end
 
-    def execute
       switches = {}
-      switches['schema-only'] = nil if @options[:structure_only]
+      switches['schema-only'] = nil if options[:structure_only]
       switches['no-privileges'] = nil
       switches['no-owner'] = nil
-      switches['host'] = @options[:host] if @options[:host].present?
-      switches['username'] = @options[:username] if @options[:username].present?
-      switches[:file] = Shellwords.escape(@options[:file])
+      switches['host'] = options[:host] if options[:host].present?
+      switches['username'] = options[:username] if options[:username].present?
+      switches[:file] = Shellwords.escape(options[:file])
 
-      if @options[:schemas]
-        schemas = @options[:schemas].split(',')
+      if options[:schemas]
+        schemas = options[:schemas].split(',')
         schemas_switches = schemas.map do |part|
           Utils.command_line_switches_from({schema: Shellwords.escape(part) })
         end
       end
 
       command_parts = []
-      if @options[:password].present?
-        command_parts << "PGPASSWORD=#{@options[:password]}"
+      if options[:password].present?
+        command_parts << "PGPASSWORD=#{options[:password]}"
       end
       command_parts += ['pg_dump',
                        Utils.command_line_switches_from(switches),
                        schemas_switches,
-                       @options[:database]]
+                       options[:database]]
       command_parts.compact.join(' ')
     end
 
